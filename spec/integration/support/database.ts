3ba583f273ecc74c4cb475f3d57bdf9ee4968eaf
mck@WSL <micka.daoud@epitech.eu>
2026-09-18 16:13:46 +0200

    fix: integration tests

--- spec/integration/support/database.ts
+++ spec/integration/support/database.ts
@@ -30,6 +30,7 @@ export interface OutboxRow {
 export async function connect(): Promise<Connection> {
     const connection = await mysql.createConnection({
         host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT ? Number(process.env.MYSQL_PORT) : undefined,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
         database: process.env.MYSQL_DB,
